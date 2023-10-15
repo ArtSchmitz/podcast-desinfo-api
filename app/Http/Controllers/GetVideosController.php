dd40ef1ffb974605bdb29545852da01f5e40533d
Arthur Felipe Schmitz <arthurfschmitz22@gmail.com>
2023-10-15 20:29:31 -0300

    create getvideos

--- app/Http/Controllers/GetVideosController.php
+++ app/Http/Controllers/GetVideosController.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use App\Models\Video;
 
 class GetVideosController extends Controller
 {
+    
+    public function index()
+    {
+        $videos = Video::all();
+
+        return view('videos.index', compact('videos'));
+    }
+
     public function getVideosDesinfo()
     {
         $apiKey = config('services.youtube.api_key');
@@ -21,8 +30,25 @@ class GetVideosController extends Controller
             'maxResults' => 10,
         ]);
 
-        $videos = $response->json(['items']);
+        if ($response->successful()) {
+            $data = $response->json();
+            $videos = $data['items'];
+    
+            $videoData = [];
+        
+            foreach ($videos as $video) {
+                $videoData[] = [
+                    'title' => $video['snippet']['title'],
+                    'description' => $video['snippet']['description'],
+                    'video_id' => $video['id']['videoId'],
+                    'video_url' => 'https://www.youtube.com/watch?v=' . $video['id']['videoId'],
+                    'published_at' => $video['snippet']['publishedAt'],
+                ];
+            }
 
-        dd($response);
+            return response()->json($videoData);
+        }else {
+            return response()->json(['error' => 'Erro na solicitação para o YouTube API']);
+        }
     }
 }
